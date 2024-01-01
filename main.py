d8bc5152d6724924a1593c8b97ffdac7746f2060
Vvender <vvender90@gmail.com>
2024-01-01 17:36:01 +0300

    utility event handler header utilities added mainwindow class created instance created for eventhandler and header button clicks.

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from Gui.ui_bookstore import Ui_Dashboard
+from EventHandler.utility_event_handler import UtilityEventHandler
+
+
+class MainWindow:
+
+    def __init__(self):
+        # Setting Currency Data (json)
+        self.app = QApplication(sys.argv)
+        self.ui_window = QWidget()
+        self.ui = Ui_Dashboard()
+        self.ui.setupUi(self.ui_window)
+        self.ui_window.show()
+
+        # Create an instance of EventHandler and pass the app and ui objects
+        self.utility_events = UtilityEventHandler(self.app, self.ui)
+
+        # UTILITY EVENTS #
+        self.ui.btn_github.clicked.connect(self.utility_events.utility_event_handler)
+        self.ui.btn_linkedin.clicked.connect(self.utility_events.utility_event_handler)
+        self.ui.btn_cv.clicked.connect(self.utility_events.utility_event_handler)
+        self.ui.btn_email.clicked.connect(self.utility_events.utility_event_handler)
+        self.ui.btn_minimize.clicked.connect(self.ui_window.showMinimized)
+        self.ui.btn_close.clicked.connect(self.app.quit)
+
+
+if __name__ == "__main__":
+    main_app = MainWindow()
+    sys.exit(main_app.app.exec_())
